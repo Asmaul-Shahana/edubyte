9d41caa24bebe38705255d0a851cc771834cb20f
Asmaul-Shahana <jucse28.360@gmail.com>
2023-04-26 01:18:26 +0600

    blogs css added

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Blog.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBookmark } from '@fortawesome/free-solid-svg-icons';
 
 const Blog = ({ blog }) => {
     const { img, author, title, dp, publish, time } = blog;
@@ -9,15 +11,19 @@ const Blog = ({ blog }) => {
             <div>
                 <div className='blog-detail'>
                     <div className='author-detail'>
-                    <img className='author-dp' src={dp} alt="profile" />
-                    <div>
-                        <h4>{author}</h4>
-                        <p>{publish} (a month ago)</p>
+                        <img className='author-dp' src={dp} alt="profile" />
+                        <div>
+                            <h4>{author}</h4>
+                            <p>{publish} (a month ago)</p>
+                        </div>
                     </div>
+                    <div className='bookmark-sec'>
+                        <h6>{time} min read</h6>
+                       <button> <FontAwesomeIcon icon={faBookmark} /></button>
                     </div>
-                    <h6>{time} min read</h6>
                 </div>
-                
+                <h2>{title}</h2>
+                <button className='mark-read-btn'> <a href="">Mark as read</a></button>
             </div>
         </div>
     );
