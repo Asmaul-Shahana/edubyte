0d25ba8b48c67acf8250298a512f43b100edbf1f
Asmaul-Shahana <jucse28.360@gmail.com>
2023-04-25 22:26:34 +0600

    Header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
 
   return (
       <div className='App'>
-       <h1>Hello world</h1>
+          <Header></Header>
       </div>
   )
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <div className="header">
+            <h2>EduByte</h2>
+            <img src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80" alt="user" />
+        </div>
+    );
+};
+
+export default Header;
